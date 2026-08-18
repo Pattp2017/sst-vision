2380ea3478519eb136d19e9bc463cd3a8f7e81e4
Pattp2017 <pafptp@gmail.com>
2026-08-18 13:08:47 -0300

    Increase JSON body limit to 10mb

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "10mb"
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({
